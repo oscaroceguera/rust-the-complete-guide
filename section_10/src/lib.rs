f5e21dba35fd14b610a730b055e93ad7a9d05002
Oscar E. Oceguera Bibriesca <oscar.oceguera@payclip.com>
2023-12-13 16:58:14 -0700

    Section 10

---
+++ section_10/src/lib.rs
@@ -0,0 +1,29 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        // assert_eq!(result, 4)
+        assert_ne!(result, 5)
+    }
+    #[test]
+    // #[ignore]
+    #[should_panic]
+    fn it_fails() {
+        panic!("Test failed!");
+    }
+    #[test]
+    fn call_simple_add() {
+        assert!(simple_add());
+    }
+}
+
+fn simple_add() -> bool {
+    if 2 + 2 == 4 {
+        true
+    } else {
+        false
+    }
+}
